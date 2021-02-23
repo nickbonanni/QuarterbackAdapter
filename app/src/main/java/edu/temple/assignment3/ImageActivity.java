cd0e7dad58fadac3f0dbb918364a5339e3c2945f
nickbonanni <nickbo514@yahoo.com>
2021-02-23 18:53:28 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/assignment3/ImageActivity.java
@@ -0,0 +1,29 @@
+package edu.temple.assignment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+public class ImageActivity extends AppCompatActivity {
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+    }
+}
