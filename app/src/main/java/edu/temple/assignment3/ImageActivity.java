4892b7c3d000c26abad3fdf5e25a645d4890c5af
nickbonanni <nickbo514@yahoo.com>
2021-02-23 19:26:19 -0500

    adding arrays and images (not functional)

--- app/src/main/java/edu/temple/assignment3/ImageActivity.java
+++ app/src/main/java/edu/temple/assignment3/ImageActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class ImageActivity extends AppCompatActivity {
 
     Spinner spinner;
+    ImageView qbImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +22,29 @@ public class ImageActivity extends AppCompatActivity {
 
         spinner = (Spinner) findViewById(R.id.spinner);
 
+        ArrayList<String> nameArray = new ArrayList<String>();
+        nameArray.add(getString(R.string.brady));
+        nameArray.add(getString(R.string.brees));
+        nameArray.add(getString(R.string.mahomes));
+        nameArray.add(getString(R.string.rodgers));
+        nameArray.add(getString(R.string.watson));
+
+       int[] qbImageArray = new int[]{R.drawable.brady, R.drawable.brees, R.drawable.mahomes,
+                                        R.drawable.rodgers, R.drawable.watson};
+
+       QuarterbackAdapter adapter = new QuarterbackAdapter(this, nameArray);
+
+       spinner.setAdapter(adapter);
+
+        /*
         spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
             }
         });
+        */
+
 
     }
 }
--- app/src/main/java/edu/temple/assignment3/QuarterbackAdapter.java
+++ app/src/main/java/edu/temple/assignment3/QuarterbackAdapter.java
@@ -4,28 +4,42 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class QuarterbackAdapter extends BaseAdapter {
 
     Context context;
+    ArrayList<String> names;
+
+    public QuarterbackAdapter(Context context, ArrayList<String> names) {
+        this.context = context;
+        this.names = names;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return names.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return names.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
+        LinearLayout linearLayout;
+        TextView qbTextView;
+
         return null;
     }
 
